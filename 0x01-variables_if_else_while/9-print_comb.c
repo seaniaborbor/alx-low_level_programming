6fb8dada2e3a3be7b733bb49bac0941c48d532be
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-11 19:38:57 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main - Print single digit numbers in the formart
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Description: Sepaeated by commas
- * Return: Always(0) Success
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 48; i < 58; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(i);
-	}
-	if (i != 57)
-	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
 }
-
