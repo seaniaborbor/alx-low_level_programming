e118de41e6d285437d689e6bb6e4e4590f02c7b4
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-11 20:29:31 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,20 @@
-/*
- * File: 9-print_comb.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
+#include <ctype.h>
 
-/**
- * main - Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0.
- */
+/*
+*main - prints single digit number comma and spaces 
+*
 int main(void)
 {
-	 int i, n;
-    for (n = 1; n < 1000000; n++) {
-        for (i = n;;) {
-            if (i / 10 % 10 > i % 10) break;
-            if ((i /= 10) == 0) { putchar("%d\n", n); break; }
-        }
-    }
-
-	return (0);
+int a;
+for(a = 0; a<10; a++)
+{
+putchar((a)+'0');
+if(a == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return(0)
 }
