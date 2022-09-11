85881c9d78be9f471965c92e6411f1be431944a3
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-11 19:31:21 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
-
 /**
-* * main - print single digit numbers
-* *
-* * Description: print single digit numbers with commas
-* *
-* * Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
+int k;
 
-	i = 48;
-
-	while (i < 58)
-	{
-		putchar(i);
-		if (i != 57)
-		{
-			putchar(44);
-			putchar(32);
-		}
-		i++;
-	}
-
-	putchar(10);
-
-	return (0);
+for (k = 48; k <= 57; k++)
+{
+putchar(k);
+if (k < 57)
+{
+putchar(',');
+putchar(' ');
 }
-
-
+}
+putchar('\n');
+return (0);
+}
