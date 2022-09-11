425578d759c91410497c7ff7ac20b2f3b30259ec
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-11 19:58:52 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,19 +12,13 @@
  */
 int main(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
+	 int i, n;
+    for (n = 1; n < 1000000; n++) {
+        for (i = n;;) {
+            if (i / 10 % 10 > i % 10) break;
+            if ((i /= 10) == 0) { putchar("%d\n", n); break; }
+        }
+    }
 
 	return (0);
 }
