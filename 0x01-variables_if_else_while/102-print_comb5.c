064db01066cb6c98f0e91101266c5ab7d710a386
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-13 01:13:26 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,29 +7,29 @@
 
 int int main(void)
 {
-	int n1 = 0, n2;
-	while(n1 <= 99)
-	{
-		n2 = n1;
-		while(n2<=99)
-		{
-			if(n2 != n1)
-			{
-				putchar((n1/10)+0);
-				putchar((n1%10)+0);
-				putchar((' '));
-				putchar((n2/10)+0);
-				putchar((n2%10)+0);
-				if(n1 != 98 || n2 != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			++n2;
-		}
-		++n1;
-	}
-	putchar('\n');
-	return 0;
+int n1 = 0, n2;
+while(n1 <= 99)
+{
+n2 = n1;
+while(n2<=99)
+{
+if(n2 != n1)
+{
+putchar((n1/10)+0);
+putchar((n1%10)+0);
+putchar((' '));
+putchar((n2/10)+0);
+putchar((n2%10)+0);
+if(n1 != 98 || n2 != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+++n2;
+}
+++n1;
+}
+putchar('\n');
+return 0;
 }
