9044c4c944b330d61f36ede52a6732dd64017228
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-15 00:39:38 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,22 @@
 #include "main.h"
-<<<<<<< HEAD
-=======
 
->>>>>>> de046805abd5df03a007a7f7fa0c3344e5590fec
 /**
- * main - Entry point
- *
- * Description: prints _putchar using putchar prototype
- *
- * Return: Always 0 (Success)
-*/
+ * main - print putchar
+ * Return: always 0
+ */
 
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
+
 }
