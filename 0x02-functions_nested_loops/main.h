5f2adc0ede6704d36922877251c6a548395e2f69
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-09-15 01:05:10 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-#ifndef HOLBERTON_H_INCLUDED
-
-#define HOLBERTON_H_INCLUDED
-
+#ifndef main_h
+#define main_h
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,8 +10,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n)
 #endif
