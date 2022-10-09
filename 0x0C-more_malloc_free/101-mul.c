810e1b1504e159c43638c661ef870dd23d928eff
Tarnue Pythagoras Borbor <111107480+seaniaborbor@users.noreply.github.com>
2022-10-09 02:07:49 +0000

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
+* main - multiply 2 numbers as arguments
+* @argc: number of arguments
+* @argv: arguments
+* Return: 0 success
+*/
 int main(int argc, char *argv[])
 {
+
 unsigned long mul;
 int i, j;
 	if (argc != 3)
@@ -23,7 +26,7 @@ int i, j;
 		}
 
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
+	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
